bc1b4bf294a06f281514f625953531dd2e0f013b
Brian Zhang <brianzhang01@users.noreply.github.com>
2019-10-09 11:35:50 +0200

    Proof of concept of lexicographic sorting

---
+++ sort.py
@@ -0,0 +1,39 @@
+"""msprime with lexicographic sort?
+
+TODO: generalize to non-binary trees
+"""
+import msprime
+
+# Great parameters! Save them
+ts = msprime.simulate(sample_size=10, length=1e6, random_seed=1,
+    mutation_rate=1e-8, recombination_rate=1e-6)
+
+print(ts.draw_text())
+
+def helper(tree, node):
+    children = tree.children(node)
+    if len(children) == 0:
+        return node, [node]
+    if len(children) != 2:
+        print("That's very interesting, node " + str(node) + " has " + len(children) + " children")
+    # children_return_values = []
+    # for child in children:
+    #     a, b = helper(tree, child)
+    #     children_return_values.append([a, b])
+    # List comprehension for elegance
+    children_return_values = [helper(tree, child) for child in children]
+    children_return_values.sort()
+    # print(children_return_values)
+    min_lexi = []
+    for element in children_return_values:
+        min_lexi += element[1]
+    return children_return_values[0][0], min_lexi
+
+def lexi(tree):
+    """Given a tree, return a list of samples producing this tree that has
+    minimum lexicographic order.
+    """
+    return helper(tree, tree.root)[1]
+
+for tree in ts.trees():
+    print(lexi(tree))
